2cfc7b640f3c14383a73049736f83d6f5af1ec29
Ward Abbass <swabbass@gmail.com>
2021-02-24 00:32:11 +0200

    Update ReactView.java

--- lib/android/app/src/main/java/com/reactnativenavigation/react/ReactView.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/react/ReactView.java
@@ -5,36 +5,35 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.MotionEvent;
 
+import androidx.annotation.RestrictTo;
+
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.uimanager.JSTouchDispatcher;
 import com.facebook.react.uimanager.UIManagerModule;
 import com.facebook.react.uimanager.events.EventDispatcher;
-import com.reactnativenavigation.viewcontrollers.viewcontroller.ScrollEventListener;
-import com.reactnativenavigation.react.events.ComponentType;
 import com.reactnativenavigation.react.events.EventEmitter;
 import com.reactnativenavigation.viewcontrollers.viewcontroller.IReactView;
+import com.reactnativenavigation.viewcontrollers.viewcontroller.ScrollEventListener;
 import com.reactnativenavigation.views.component.Renderable;
-
-import androidx.annotation.RestrictTo;
 
 @SuppressLint("ViewConstructor")
 public class ReactView extends ReactRootView implements IReactView, Renderable {
 
-	private final ReactInstanceManager reactInstanceManager;
-	private final String componentId;
-	private final String componentName;
-	private boolean isAttachedToReactInstance = false;
+    private final ReactInstanceManager reactInstanceManager;
+    public final String componentId;
+    private final String componentName;
+    private boolean isAttachedToReactInstance = false;
     private final JSTouchDispatcher jsTouchDispatcher;
 
     public ReactView(final Context context, ReactInstanceManager reactInstanceManager, String componentId, String componentName) {
-		super(context);
-		this.reactInstanceManager = reactInstanceManager;
-		this.componentId = componentId;
-		this.componentName = componentName;
-		jsTouchDispatcher = new JSTouchDispatcher(this);
-	}
+        super(context);
+        this.reactInstanceManager = reactInstanceManager;
+        this.componentId = componentId;
+        this.componentName = componentName;
+        jsTouchDispatcher = new JSTouchDispatcher(this);
+    }
 
     @Override
     protected void onAttachedToWindow() {
@@ -45,47 +44,34 @@ public class ReactView extends ReactRootView implements IReactView, Renderable {
     public void start() {
         if (isAttachedToReactInstance) return;
         isAttachedToReactInstance = true;
-		final Bundle opts = new Bundle();
-		opts.putString("componentId", componentId);
-		startReactApplication(reactInstanceManager, componentName, opts);
-	}
-
-	@Override
-	public boolean isReady() {
-		return isAttachedToReactInstance;
-	}
-
-	@Override
-	public ReactView asView() {
-		return this;
-	}
-
-	@Override
-	public void destroy() {
-		unmountReactApplication();
-	}
-
-	public void sendComponentStart(ComponentType type) {
-        ReactContext currentReactContext = reactInstanceManager.getCurrentReactContext();
-        if (currentReactContext != null) {
-            new EventEmitter(currentReactContext).emitComponentDidAppear(componentId, componentName, type);
-        }
-	}
-
-	public void sendComponentStop(ComponentType type) {
-        ReactContext currentReactContext = reactInstanceManager.getCurrentReactContext();
-        if (currentReactContext != null) {
-            new EventEmitter(currentReactContext).emitComponentDidDisappear(componentId, componentName, type);
-        }
-	}
+        final Bundle opts = new Bundle();
+        opts.putString("componentId", componentId);
+        startReactApplication(reactInstanceManager, componentName, opts);
+    }
 
     @Override
-	public void sendOnNavigationButtonPressed(String buttonId) {
+    public boolean isReady() {
+        return isAttachedToReactInstance;
+    }
+
+    @Override
+    public ReactView asView() {
+        return this;
+    }
+
+    @Override
+    public void destroy() {
+        unmountReactApplication();
+    }
+
+
+    @Override
+    public void sendOnNavigationButtonPressed(String buttonId) {
         ReactContext currentReactContext = reactInstanceManager.getCurrentReactContext();
         if (currentReactContext != null) {
-            new EventEmitter(currentReactContext).emitOnNavigationButtonPressed(componentId, buttonId);
+             EventEmitter.INSTANCE.emitOnNavigationButtonPressed(componentId, buttonId);
         }
-	}
+    }
 
     @Override
     public ScrollEventListener getScrollEventListener() {
